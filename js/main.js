446bf55d3d31fdb32af0dfbf518ee5a3575dabb5
Andrey <shock.work@mail.ru>
2020-10-15 11:10:57 +0300

    #Adaptivity 100%

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ $(function () {
         slidesToShow: 2,
         slidesToScroll: 2,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1106,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     })
 
     $('.btn__menu').on('click', function () {
